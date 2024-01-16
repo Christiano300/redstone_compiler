7eae196655c789eb39e7e17ba30434b1e0ce06a3
Christiano300 <christian.patzl@gmx.net>
2024-01-16 21:12:56 +0100

    Redo part of eval parsing

--- src/backend/compiler.rs
+++ src/backend/compiler.rs
@@ -4,16 +4,23 @@ use crate::frontend::{Code, Expression, Operator, Parser, Statement};
 
 use super::{Instruction, InstructionVariant};
 
+pub enum CompilerError {
+    NonexistentVar(String),
+    TooManyVars,
+}
+
+type Res<T = ()> = Result<T, CompilerError>;
+
 macro_rules! instr {
     ($self:ident, $variant:ident, $arg:expr) => {
         $self.instructions.push(Instruction {
-            variant: InstuctionVariant::$variant,
+            variant: &InstructionVariant::$variant,
             arg: Some($arg),
         })
     };
     ($self:ident, $variant:ident) => {
         $self.instructions.push(Instruction {
-            variant: InstructionVariant::$variant,
+            variant: &InstructionVariant::$variant,
             arg: None,
         })
     };
@@ -92,21 +99,41 @@ impl Compiler {
         Err(())
     }
 
-    fn insert_var(&mut self, symbol: String) {
+    fn insert_var(&mut self, symbol: &String) -> Res<u8> {
         let last_scope = self.scopes.last_mut().unwrap();
-        last_scope
-            .variables
-            .insert(symbol, last_scope.variables.len().try_into().unwrap());
+        let slot = match last_scope.variables.len().try_into() {
+            Ok(v) => v,
+            Err(_) => return Err(CompilerError::TooManyVars),
+        };
+        last_scope.variables.insert(symbol.clone(), slot);
+        Ok(slot)
     }
 
-    fn get_var(&self, symbol: &String) -> Result<u8, ()> {
+    fn get_var(&self, symbol: &String) -> Res<u8> {
         for scope in self.scopes.iter().rev() {
             let entry = scope.variables.get(symbol);
             if let Some(v) = entry {
                 return Ok(*v);
             }
         }
-        Err(())
+        Err(CompilerError::NonexistentVar(symbol.clone()))
+    }
+
+    fn insert_temp_var(&mut self) -> Res<(u8)> {
+        let last_scope = self.scopes.last_mut().unwrap();
+        let slot = match last_scope.variables.len().try_into() {
+            Ok(v) => v,
+            Err(_) => return Err(CompilerError::TooManyVars),
+        };
+        last_scope.variables.insert(format!(" {}", slot), slot);
+        Ok(slot)
+    }
+
+    fn cleanup_temp_var(&mut self, index: u8) {
+        let last_scope = self.scopes.last_mut().unwrap();
+        last_scope
+            .variables
+            .remove(format!(" {}", index);
     }
 
     fn generate_assembly(mut self, body: Vec<Code>) -> Vec<Instruction> {
@@ -153,7 +180,7 @@ impl Compiler {
         }
     }
 
-    fn eval_expr(&mut self, expr: Expression) {
+    fn eval_expr(&mut self, expr: &Expression) -> Res {
         match expr {
             Expression::NumericLiteral(number) => todo!(),
             Expression::Identifier(ident) => todo!(),
@@ -161,15 +188,100 @@ impl Compiler {
                 left,
                 right,
                 operator,
-            } => self.eval_binary_expr(&*left, &*right, operator),
+            } => self.eval_binary_expr(&left, &right, operator)?,
             _ => {}
+        }
+        Ok(())
+    }
+
+    fn is_simple(expr: &Expression) -> bool {
+        use Expression::*;
+        match expr {
+            Identifier(..) | NumericLiteral(..) => true,
+            BinaryExpr {
+                left,
+                right,
+                operator,
+            } => Compiler::is_simple(left) && Compiler::is_simple(right),
+            _ => false,
         }
     }
 
-    fn eval_binary_expr(&mut self, left: &Expression, right: &Expression, operator: Operator) {
+    fn eval_binary_expr(
+        &mut self,
+        left: &Expression,
+        right: &Expression,
+        operator: &Operator,
+    ) -> Res {
+        use Expression::*;
         match (left, right) {
+            (Identifier(..) | NumericLiteral(..), Identifier(..) | NumericLiteral(..)) => {
+                self.eval_simple_expr(left, right, operator)?;
+            }
+            (Identifier(..) | NumericLiteral(..), _) | (_, Identifier(..) | NumericLiteral(..)) => {
+            }
             _ => {}
         }
+        Ok(())
+    }
+
+    fn eval_assignment(&mut self, symbol: &String, value: &Expression) -> Res {
+        let slot = self.insert_var(symbol)?;
+
+        self.eval_expr(value)?;
+
+        instr!(self, SVA, slot);
+
+        Ok(())
+    }
+
+    fn eval_simple_expr(
+        &mut self,
+        left: &Expression,
+        right: &Expression,
+        operator: &Operator,
+    ) -> Res {
+        // load a
+        use Expression::*;
+        match left {
+            NumericLiteral(value) => {
+                let bytes = value.to_le_bytes();
+                instr!(self, LAL, bytes[0]);
+                if bytes[1] != 0 {
+                    instr!(self, LAH, bytes[1]);
+                }
+            }
+            Identifier(symbol) => {
+                instr!(self, LA, self.get_var(symbol)?);
+            }
+            _ => {}
+        }
+
+        match right {
+            NumericLiteral(value) => {
+                let bytes = value.to_le_bytes();
+                instr!(self, LBL, bytes[0]);
+                if bytes[1] != 0 {
+                    instr!(self, LBH, bytes[1]);
+                }
+            }
+            Identifier(symbol) => {
+                instr!(self, LB, self.get_var(symbol)?);
+            }
+            _ => {}
+        }
+
+        use Operator::*;
+        match operator {
+            Plus => instr!(self, ADD),
+            Minus => instr!(self, SUB),
+            Mult => instr!(self, MUL),
+            And => instr!(self, AND),
+            Or => instr!(self, OR),
+            Xor => instr!(self, XOR),
+        }
+
+        Ok(())
     }
 
     fn parse_expression(&mut self, value: Expression) {}
